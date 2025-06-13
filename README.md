71965dbe6ec959b925a32ef6e40ec690f6dbfe09
Val <val.cb@ciencias.unam.mx>
2025-06-13 11:58:30 -0600

    two list func

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Data structures and some algorithms in prolog.
+
+## Lists:
+
+Basic defintions of algorithms for lists:
+
+### append(+List, +Element, -ResultantList)
+
+Appends an element at the end of the list.
+
+### delete(+List, +Element, -ResultantList)
+
+Deletes a certain element from de list.
--- src/list.pl
+++ src/list.pl
@@ -1,3 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Definición de listas.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+append([], E, [E]).
+append([X|L1], E, [X|L3]) :- append(L1, E, L3).
+
+delete([], _, []).
+delete([E|Xs], E, Xs).
+delete([X|Xs], E, [X|Ys]) :- X \= E, delete(Xs, E, Ys).
